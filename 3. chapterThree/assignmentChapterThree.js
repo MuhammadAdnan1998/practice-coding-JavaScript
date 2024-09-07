f222976a8db5215ed46eba461f3a5f9c5d6db768
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-07 15:17:32 +0500

    Chapter three tasks completed

---
+++ 3. chapterThree/assignmentChapterThree.js
@@ -0,0 +1,62 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 3 (Variables for numbers)
+// ______________________________________________________________
+
+
+// Q1
+// Declare a variable “caseQty”
+
+var caseQty;
+
+
+// Q2
+// Assign to the variable caseQty, which has already been declared,the 
+// value 144.
+
+var caseQty = 144;
+
+console.log(caseQty);
+
+
+//Q3
+// Rewrite this statement so the variable can be used in a math operation.
+//  var num = "9";
+
+var num ="9";
+
+console.log(num);
+
+
+//Q4
+// In one statement declare a variable. In a second statement assign it 
+// the sum of 2 numbers.
+
+var num1 = 10;
+
+console.log(num1 + 4);
+
+
+//Q5
+// What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal);
+
+
+//Q6
+// In the first statement declare a variable and assign it a number. In
+// the second statement, change the value of the variable by adding
+// it together with a number.
+
+var num2 = 8;
+var num3 = num1 + 5;
+
+console.log(num3);
