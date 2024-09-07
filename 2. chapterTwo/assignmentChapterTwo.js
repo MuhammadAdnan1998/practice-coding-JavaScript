75dcde8d00415d29b6ec68ef22b3f74ad3fdb884
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-07 15:16:53 +0500

    Chapter two tasks completed

---
+++ 2. chapterTwo/assignmentChapterTwo.js
@@ -0,0 +1,39 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 2 (Variables for string)
+// ______________________________________________________________
+
+// Q1
+// Declare any variable in the camelCase format
+
+var firstName = "Muhammad";
+var lastName = "Adnan";
+alert("My name is" + firstName + " " + lastName);
+
+
+// Q2
+// Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+
+var studentName;
+studentName = "MuhammadAdnan";
+console.log(studentName);
+
+
+// Q3
+// Declare the variable teamName and Alert your Team name.
+
+var  teamName;
+teamName = prompt("Your team in PSL 2024 :");
+alert("Your team in PSL 2024 is :" + teamName )
+
+// Output: My name is Muhammad Adnan and I am 25 years old.
+
+
+// Q4
+// This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string.
+
+var bestMan = "Charlie";
+var fullName = "Charlie Chaplin";
+bestMan == fullName;
+console.log(bestMan);
+
