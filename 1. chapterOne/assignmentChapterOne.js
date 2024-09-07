e40b6dc4afdb001edb257a5ea8c989def41bbbd6
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-07 15:13:55 +0500

    Chapter one tasks completed

---
+++ 1. chapterOne/assignmentChapterOne.js
@@ -0,0 +1,31 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 1 (Alerts)
+// ______________________________________________________________
+
+// Q1
+// Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V. Password
+
+let firstName = prompt("What is your First Name ?") ;
+let lastName = prompt("What is your Last Name ?") ;
+let email = prompt("What is your Email ?") ;
+let phoneNumber = prompt("What is your Phone Number ?") ;
+let password = prompt("What is your Password ?") ;
+
+alert("Your name is " + firstName + " " + lastName + " " + "Your email is " + email + " and your phone number & password are " + phoneNumber + ", " + password + ".");
+
+// Q2
+// Correct this statement: alert"You're learning JavaScript!";
+
+alert("You're learning JavaScript!");
+
+// Q3
+// Code an alert statement displaying any message you like.
+
+alert("Hello! I am an alert box!\nHow are you?");
+
