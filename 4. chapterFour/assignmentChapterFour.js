21d28e4e47b16e26f3eb6f6673e2d6b7fd8835ed
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-07 15:18:13 +0500

    Chapter four tasks completed

---
+++ 4. chapterFour/assignmentChapterFour.js
@@ -0,0 +1,55 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 4 (Variable names Legal and Illegal)
+// ______________________________________________________________
+
+
+// Q1
+// Correct this statement.
+// var product cost = 3.45;
+
+var productCost = 3.45; //space is not allowed
+
+
+// Q2
+// Rewrite this using camelCase.
+// var Nameofband;
+
+var nameOfBand;
+
+
+// Q3
+// In a single statement declare a legally-named variable and assign a
+// number to it.
+
+var num1 = 5;
+
+console.log(num1);
+
+
+// Q4
+// Declare a variable that is a combination of your first and last
+// names. Use camelCase.
+
+var firstName = "Muhammad";
+var lastName = "Adnan";
+var fullName = firstName + " " + lastName;
+
+console.log("My name is", fullName);
+
+
+// Q5
+// List the legal and Illegal Variables.
+
+// asd -------->   legal
+// 123 -------->   illegal
+// ab12 -------->   legal
+// 12ab -------->   illegal
+// var -------->   illegal
+// ab@ -------->   illegal
+// ab_ -------->   legal
+
+
+
+
+
