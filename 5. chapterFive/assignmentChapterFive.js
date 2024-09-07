b594f0f095ab81e15162ffc2bc5e57f3677122da
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-07 15:19:31 +0500

    Chapter five tasks completed

---
+++ 5. chapterFive/assignmentChapterFive.js
@@ -0,0 +1,60 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 5 (Math Expression I)
+// ______________________________________________________________
+
+
+// Q1
+// What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+
+// The name of arithmetic operator that gives remainder is modulus operator,its symbol is % 
+
+
+// Q2
+// What is the value of num?
+// var num = 20 % 6;
+
+var num = 20 % 6; //20 % 6 = 2
+
+
+// Q3
+// In a single statement, declare the variable largeNum and assign it
+// the result of 1,000 multiplied by 2,000.
+
+var largeNum = 1000;
+
+console.log(largeNum*2000);
+
+
+// Q4
+// Assign to a variable the value represented by one variable
+// subtracted from the value represented by another variable
+
+var num1 = 8;
+var num2 = 5;
+
+console.log(num2 - num1);
+
+
+// Q5
+// Assign to a variable the remainder when one number is divided by
+// another. The variable hasn't been declared beforehand. Make up
+// the variable name.
+
+var leftOver = 10 % 3;
+
+console.log(leftOver);
+
+
+// Q6
+// Code an alert that displays the result of a multiplication on 2
+// numbers.
+
+var num1 = 4;
+var num2 = 6;
+var num3 = num1*num2;
+alert(num3);
+
+
+
