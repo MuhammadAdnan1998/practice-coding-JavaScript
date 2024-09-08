291b9f5a440be52c2246e7643700a7d4ae83d345
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-08 12:01:24 +0500

    Chapter ten tasks completed

---
+++ 10. chapterTen/assignmentChapterTen.js
@@ -0,0 +1,58 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 10 (if statments)
+// ______________________________________________________________
+
+// // Q1
+// var city = "Karachi"
+// if (city = "Karachi") {
+//     console.log("The City OF Lights")
+//     Correct the above statement:
+//     Also try this statement by yourself
+
+// var city = "Karachi"
+// if (city = "Karachi") {
+//     console.log("The City OF Lights")
+// }
+
+
+// // Q2
+// This is the first line of an if statement:
+// if (x === y) {
+// Complete the statement. If the condition is true, display a box
+// that asks the user value of z? and assign it to another variable
+
+// var x = +prompt("Enter the value of x");
+// var y = +prompt("Enter the value of y");
+// if (x === y) {
+//   var z = +prompt("Enter the value of z");
+//   var a = z;
+//   console.log("Assign user value of z to variable a : ", a);
+// } else {
+//   console.log("Condition is not true");
+// }
+
+
+// // Q3
+// Code an if statement that tests if ZipCode is "10010" so, Alert
+// that "Karachi". if not then alert ("Please write correct city")
+
+// var zipCode = +prompt("Enter the ZipCode");
+// if (zipCode == 10010) {
+//     alert("Welcome to Karachi")
+// } else {
+//     alert ("Please write correct city")
+// }
+
+
+// // Q4
+// Code an if statement. Test whether a variable has a particular
+// numerical value. If so, assign a new value to that variable, as in
+// x = 1;
+
+var a ="2";
+if ( a ==="2"){
+    var b = prompt("Enter a new value :");
+    b == a;
+    console.log("Assign a new value to variable a :",a)
+}
