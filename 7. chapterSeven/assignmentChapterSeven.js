f5f320acef321ba1f13dd3766d1bb37b9651c599
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-08 11:26:21 +0500

    Chapter seven tasks completed

---
+++ 7. chapterSeven/assignmentChapterSeven.js
@@ -0,0 +1,72 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 7 (Math Expression III)
+// ______________________________________________________________
+
+
+// // Q1
+// // var calculatedNum = 2 + (2 * 6);
+// // What is the value of calculatedNum?
+
+var calculatedNum = 2 + (2 * 6);
+console.log(calculatedNum);
+// //Output: the value of calculatedNum is 14.
+
+
+// // Q2
+// // var calculatedNum = (2 + 2) * 6;
+// // What is the value of calculatedNum?
+
+var calculatedNum = (2 + 2) * 6;
+console.log(calculatedNum);
+// //Output: the value of calculatedNum is 24.
+
+
+// // Q3
+// // var calculatedNum = (2 + 2) * (4 + 2);
+// // What is the value of calculatedNum?
+
+var calculatedNum = (2 + 2) * (4 + 2);
+console.log(calculatedNum);
+// // Output: the value of calculatedNum is 24.
+
+
+// // Q4
+// // var calculatedNum = ((2 + 2) * 4) + 2;
+// // What is the value of calculatedNum?
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+console.log(calculatedNum);
+// // Output: the value of calculatedNum is 18.
+
+
+// Q5
+// Write a statement that assigns to cost the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 56.
+
+var costTheResult = (2 + 2) * (4 + 10);
+console.log(costTheResult);
+// // Output: the value of costTheResult is 56.
+
+
+// Q6
+// Write a statement that assigns to units the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 20.
+
+var unitsTheResult = (2 + 2 * 4 + 10);
+console.log(unitsTheResult);
+// // Output: the value of costTheResult is 20.
+
+
+// Q7
+// Write a statement that assigns to pressure the result of 4 / 2 * 4,
+// clarified with parentheses, producing .5.
+
+
+var pressureTheResult = 4 / (2 * 4);
+console.log(pressureTheResult);
+// Output: the value of costTheResult is .5.
+
+
+
+
