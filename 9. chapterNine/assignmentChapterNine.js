6f6075257189cb89548d48117905134b76f36e76
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-08 11:43:42 +0500

    Chapter nine tasks completed

---
+++ 9. chapterNine/assignmentChapterNine.js
@@ -0,0 +1,37 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 9  (Prompts)
+// ______________________________________________________________
+
+
+// // Q1
+// Code a prompt with the message "Enter first name". The user's
+// response is assigned to firstName.
+
+// var firstname = prompt("Enter your first Name")
+
+
+// // Q2
+// Code a prompt with the message "Country?" and the default
+// answer "China". The user's response is assigned to country.
+
+// var china = prompt("Enter the country", "China")
+
+
+// // Q3
+// Correct this statement
+// var yourName = prompt(Enter Your Name");
+
+// var yourName = prompt("Enter Your Name");
+
+
+// // Q4
+// Code a prompt that specifies a string as the message Include a
+// default input.
+
+// var defaultPrompt = prompt("Enter the country", "China")
+
+// var message = prompt("Enter Your message");
+// var defaultMessage = prompt("Defalut Message", "Hey")
+// var thirdVariable = message + " " + defaultMessage;
+// console.log(thirdVariable)
