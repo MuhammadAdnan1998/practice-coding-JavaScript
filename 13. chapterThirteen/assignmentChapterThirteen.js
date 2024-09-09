050aac82dd0b52f1faba78e848ee3fc8f2c11f28
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-09 16:07:24 +0500

    Chapter thirteen tasks completed

---
+++ 13. chapterThirteen/assignmentChapterThirteen.js
@@ -0,0 +1,56 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 13 (Testing sets of conditions)
+// ______________________________________________________________
+
+// // Q1
+// Code the first line of an if statement that tests whether both are
+// true: a has the same value as b and c has the same value as d.
+
+// if (a == b && c == d) {
+// }
+
+// // Q2
+// Code the first line of an if statement that tests whether either or
+// both are true: a has the same value as b or c doesn't have the
+// same value as d.
+
+// if (a == b || c != d) {
+// }
+
+// // Q3
+// Code the first line of an if statement that tests whether
+// I. name is either "Hamza" or "Arsalan".
+// II. age is Less than 60.
+
+// var a = "Hamza";
+// var b = "Arsalan";
+// var age;
+// var x;
+// if ((x == a || x == b) && age < 60) {
+// }
+
+// // Q4
+// Declare two variables and assign them number values.
+// If the first variable is less than the second variable or greater than
+// the second variable, display an alert.
+
+// var a = 4;
+// var b = 3;
+// if (a < b || a > b) {
+//   alert("Hello");
+// }
+
+// // Q5
+// Declare 2 variables. Assign one of them your first name and the
+// other one your last name.
+// Code 2 prompts, asking for your first and your last name.
+// If your answers match the two variables, display an alert.
+
+// var firstName = "Muhammad";
+// var lastName = "Adnan";
+// var userDeclare_firstName = prompt("Enter First Name: ");
+// var userDeclare_lastName = prompt("Enter Last Name: ");
+// if (firstName == userDeclare_firstName && lastName == userDeclare_lastName) {
+//   alert("Your name is matched.");
+// }
