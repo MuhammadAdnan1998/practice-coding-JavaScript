5fcac98594ac92144d64423d2d2968a2d423c28c
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-09 20:00:31 +0500

    Chapter fourteen tasks completed

---
+++ 14. chapterFourteen/assignmentChapterFourteen.js
@@ -0,0 +1,61 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 14 (If statements nested)
+// ______________________________________________________________
+
+// // Q1
+// Code an if statement enclosing a nested if. If password is not
+// empty, then check if password is not greater than 5 , then display
+// an alert that says "Password must be greater than 5" if greater
+// than 5 then Alert "OK".
+
+// var password;
+// if( password!===""){
+//     if(password !> 5){
+//         alert("Password must be greater than 5")
+//     }
+//     else if (password>5){
+//         alert("OK")
+//     }
+
+// // Q2
+// Try this statement by yourself
+// if (a === 1) {
+// if (c === "Max") {
+//  alert("OK");
+//  }
+// }
+
+// if (a === 1) {
+//   if (c === "Max") {
+//     alert("OK");
+//   }
+// }
+
+// // Q3
+// Code the first line of an if statement that avoids the nesting above
+// by testing for multiple conditions.
+// if (a === 1) {
+//  if (c === "Max") {
+//     alert("OK");
+//  }
+// }
+
+// if (a === 1 && c === "Max") {
+//   alert("OK");
+// }
+
+// // Q4
+// Declare two variables and assign them the same number value.
+// Test two conditions, using nested if statements. Test whether the
+// first variable equals the second variable and also whether it is less
+// than or equal to the second variable. If the test passes—and it
+// will—display an alert message.
+
+// var firstVaribale = 4;
+// var secondVaribale = 4;
+// if (firstVaribale === secondVaribale) {
+//   if (firstVaribale <= secondVaribale) {
+//     alert("Condition passed");
+//   }
+// }
