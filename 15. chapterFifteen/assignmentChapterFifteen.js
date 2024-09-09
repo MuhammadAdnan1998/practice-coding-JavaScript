3787283b927b9a6a4845851a9ef9e776c2082795
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-09 22:40:10 +0500

    Chapter fifteen tasks completed

---
+++ 15. chapterFifteen/assignmentChapterFifteen.js
@@ -0,0 +1,42 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 15  (Array I)
+// ______________________________________________________________
+
+
+// // Q1
+// Declare an empty array.
+
+// var emptyArray =[];
+
+
+// // Q2
+// Code an array with 1 string element
+
+// var array1 =["Saylani Students"];
+
+
+// // Q3
+// var alphabet = ["h","i","j","k"]. Now print the letter “j” in alert
+// using array index
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]);
+
+
+// // Q4
+// var alphabet=["h","i","j","k", “l”,”m”, “n”, “o”]. Find the total
+// length of array.
+
+// var alphabet=["h","i","j","k",“l”,”m”,“n”,“o”];
+// console.log(alphabet.length);
+
+
+// // Q5
+// Declare an array with one element and Add a second element
+// with index in array. Create an alert whose message is the new
+// element.
+
+var array1 =["Muhammad Adnan"];
+array1[1]="Muhammad Bilal";
+alert(array1[1]);
