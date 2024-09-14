94dbd7d1e326ab9a31a8f737d0bfb1c4108b0c5e
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-14 21:44:45 +0500

    Chapter twenty six completed

---
+++ 20. chapterTwentySix/assignmentChapterTwentySix.js
@@ -0,0 +1,48 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 26 (Rounding Numbers)
+// ______________________________________________________________
+
+// // Q1
+// Form a statement that rounds a number to the nearest integer.
+
+// var a = 1.7;
+// var y = Math.round(a);
+// console.log(y);
+
+
+// // Q2
+// Round up a number represented by origNum and assign it to
+// roundNum, which hasn't been declared beforehand.
+
+// var origNum = 0.6;
+// var roundNum = Math.ceil(origNum);
+// console.log(roundNum);
+
+
+// // Q3
+// Round down a number represented by origNum and assign it to
+// roundNum, which hasn't been declared beforehand.
+
+// var origNum = 0.6;
+// var roundNum = Math.floor(origNum);
+// console.log(roundNum);
+
+
+
+// // Q4
+// Round a number represented by a variable and assign the result
+// to a second variable that hasn't been declared beforehand.
+
+// var origNumber = 2.5;
+// var roundNumber = Math.round(origNumber);
+// console.log(roundNumber);
+
+
+// // Q5
+// Round .5 to 0 and assign it to a variable that hasn't been declared
+// beforehand.
+
+// var origNumber = 0.5;
+// var roundNumber = Math.floor(origNumber);
+// console.log(roundNumber);
