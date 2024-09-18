5237dba4ce4e843964c01d613c733d3718f2d087
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-18 10:56:01 +0500

    Chapter thirty tasks completed

---
+++ 23. chapterThirty/assignmentChapterThirty.js
@@ -0,0 +1,50 @@
+// ______________________________________________________________
+
+// Assignment: Chapter 30 (Controlling the length of decimals)
+// ______________________________________________________________
+
+// // Q1
+// Code a statement that rounds a number represented by num to 4
+// places, converts it to a string, and assigns it to newNum, which
+// hasn't been declared beforehand.
+
+// var num = Math.random();
+// var num = num.toFixed(4);
+// newNum = num.toString();
+// console.log(newNum);
+
+// // Q2
+// In a single statement round a number represented by a variable to
+// 2 places, convert it to a string, convert it back to a number, and
+// assign it to the same variable.
+
+// var variable1 = Math.random();
+// var newVariable = variable1.toFixed(2);
+
+// console.log("Type of a variable: " + typeof (newVariable));
+// console.log("Value of a is: ", newVariable);
+
+// var newVariable = newVariable.toString();
+// console.log("Type of a variable: " + typeof (newVariable));
+// console.log("Value of a is: ", newVariable);
+
+// var variable1 = +newVariable;
+// console.log("Type of a variable: " + typeof (variable1));
+// console.log("Value of a is: ", variable1);
+
+// // Q3
+// Code the first line of an if statement that tests whether the
+// number represented by num, rounded to 2 digits and converted
+// to a string, has more than 4 characters in it.
+
+
+// // Q4
+// Assign a number with many decimal places to a variable.
+// Code an alert that displays the number rounded to 2 decimal
+// places and converted to a string.
+
+// var variable = Math.random();
+// var numberRounded = variable.toFixed(2);
+// console.log(numberRounded);
+// variable = numberRounded.toString();
+// console.log("Type of a variable: " + typeof variable);
